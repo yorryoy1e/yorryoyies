9da3829c6126a953831c577116443106a181ffce
yorryoy1e <yorrygalvina@gmail.com>
2025-05-21 22:43:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Selamat Datang di Web Informatika")
 st.write(
-    "ngodingseru bersama Bapak Hendri"
+    "ngodingseru bersama Yorry Galvina"
 )
 st.image("https://github.com/yorryoy1e/yorryoyies/blob/main/733CEC76-D5DE-450D-B04B-53D6CD90E511.png", width=200)
